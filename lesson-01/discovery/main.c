2476d691bdeba5ab9e11c17d29b11c4308d22e3e
Antonio Castro <antcs.95adra@gmail.com>
2024-06-01 17:56:28 +0200

    lesson 1 discovery board

---
+++ lesson-01/discovery/main.c
@@ -0,0 +1,27 @@
+//#include "ARMCM0.h"
+
+int main()
+{
+    int volatile counter = 0;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+}
